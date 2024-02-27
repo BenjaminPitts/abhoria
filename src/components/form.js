127bc234133b0a523f7be052d86e449e02a83688
Ben Pitts <benjaminjackpittsAgmail.com>
2024-02-27 12:43:21 -0700

    updated press list

--- src/components/form.js
+++ src/components/form.js
@@ -8,14 +8,14 @@ class Form extends React.Component {
     <form method="post" netlify-honeypot="bot-field" data-netlify="true" action="/" name="contact" netifly>
     <input type="hidden" name="bot-field" />
     <input type="hidden" name="form-name" value="contact" />
-    <label for="name">
-    <input type="text" name="name" placeholder='your name'/><br />
+    <label aria-label="name">
+    <input type="text" name="name" id='name' placeholder='your name'/><br />
     </label>
-    <label for="email">
-    <input type="email" name="email" placeholder='your email'/><br />
+    <label aria-label="email">
+    <input type="email" name="email" id="email" placeholder='your email'/><br />
     </label>
-    <label for="messsage">
-    <textarea type="text" name="message" rows='10'placeholder='message'>
+    <label aria-label="messsage">
+    <textarea type="text" name="message" id="message" rows='10'placeholder='message'>
     </textarea><br />
     </label>
     <button type='submit'>Send</button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
   <h1>Stream the new album <i>"Depths"</i></h1>
-  <h3>Released worlwide on 1/19/2024 via <a href='https://www.prostheticrecords.com/' target='blank'> Prosthetic Records</a></h3>
-  <h3>Tour dates and merchandise available at: <a href='https://abhoria.bandcamp.com/' target='blank'> https://abhoria.bandcamp.com/</a></h3>
+  <h4>Released worlwide on 1/19/2024 via <a href='https://www.prostheticrecords.com/' target='blank'> Prosthetic Records</a></h4>
+  <h4>Tour dates and merchandise available through our <a href='https://abhoria.bandcamp.com/' target='blank'> Bandcamp</a> page</h4>
 <div class='index-box'>
 <div class='index-inner-box'>
       <StaticImage
@@ -28,9 +28,13 @@ const IndexPage = () => (
       alt="Abhoria"
       style={{ margin: `1rem`, border:`2px solid black` }}
     />
+
 </div>
 <iframe style={{margin: `1rem`, width: `350px`, height: `800px`, border:`2px solid black`}} title="Abhoria-Depths" src="https://bandcamp.com/EmbeddedPlayer/album=3627524110/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=true/transparent=true/" seamless><a href="https://abhoria.bandcamp.com/album/depths">DEPTHS by Abhoria</a></iframe>
 </div>
+
+{/* <a href="https://www.songkick.com/artists/10275215" aria-label="songkick" class="songkick-widget" data-theme="dark" data-track-button="on" data-detect-style="off" data-background-color="rgb(0,0,0,1)" data-font-color="rgb(255,255,255,1)" data-button-bg-color="rgb(255,255,255,1)" data-button-text-color="rgb(0,0,0,1)" data-locale="en" data-other-artists="on" data-share-button="on" data-country-filter="on" data-rsvp="on" data-request-show="on" data-past-events="off" data-past-events-offtour="off" data-remind-me="off"></a><script src='//widget-app.songkick.com/injector/10275215'></script> */}
+  
   </Layout>
 )
 
